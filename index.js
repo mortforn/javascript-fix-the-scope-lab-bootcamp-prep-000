ed1f361c699c0af4afb4e1e6329001065f9d89d6
Matthew Fine <mfine@wesleyan.edu>
2018-09-05 04:08:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var animal = 'cat'
 
-function myAnimal() {
+function myAnimal(animal) {
   return animal
 }
 
@@ -8,7 +8,6 @@ function yourAnimal(animal) {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' belowz
-  var animal = 'cat';
   return animal;
 }
 
