c1b21031dbcf5d9bdb5a6226bacf93e72c66197f
Matthew Fine <mfine@wesleyan.edu>
2018-09-05 04:02:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var animal = ''
+var animal = 'dog'
 
-function myAnimal(animal) {
+function myAnimal() {
   return animal
 }
 
 function yourAnimal(animal) {
   // How can we make sure that this function
   // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
+  // P.S.: You can't just hard-code 'cat' belowz
   return animal
 }
 
